976bb0fb4d7b4bf99bac6aca3714e003b1aab01d
natanbc <natanbc@users.noreply.github.com>
2020-08-18 13:12:36 -0300

    move docs to json file

--- src/main/java/com/github/natanbc/tiletools/integration/JEIIntegration.java
+++ src/main/java/com/github/natanbc/tiletools/integration/JEIIntegration.java
@@ -1,19 +1,19 @@
 package com.github.natanbc.tiletools.integration;
 
-import com.github.natanbc.tiletools.init.Registration;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @JeiPlugin
 public class JEIIntegration implements IModPlugin {
@@ -25,70 +25,27 @@ public class JEIIntegration implements IModPlugin {
     
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        registration.addIngredientInfo(
-                new ItemStack(Registration.TILE_IN_A_BOTTLE.get()),
-                VanillaTypes.ITEM,
-                "message.tiletools.bottle_info1",
-                "message.tiletools.bottle_info2"
-        );
+        JsonArray array = (JsonArray)new JsonStreamParser(
+                new InputStreamReader(JEIIntegration.class.getResourceAsStream(
+                        "/assets/tiletools/jei_info.json"
+                ), StandardCharsets.UTF_8)
+        ).next();
+        for(JsonElement element : array) {
+            JsonObject obj = element.getAsJsonObject();
+            registration.addIngredientInfo(
+                    CraftingHelper.getItemStack(obj.get("target").getAsJsonObject(), true),
+                    VanillaTypes.ITEM,
+                    toMessageNames(obj.get("text"))
+            );
+        }
+    }
     
-        registration.addIngredientInfo(
-                new ItemStack(Registration.ACCELERATION_WAND.get()),
-                VanillaTypes.ITEM,
-                "message.tiletools.wand_info1",
-                "message.tiletools.wand_info2"
-        );
-    
-        registration.addIngredientInfo(
-                new ItemStack(Registration.ACCELERATOR.item()),
-                VanillaTypes.ITEM,
-                "message.tiletools.accelerator_info",
-                "message.tiletools.redstone_disable"
-        );
-    
-        registration.addIngredientInfo(
-                new ItemStack(Registration.GROWTH_ACCELERATOR.item()),
-                VanillaTypes.ITEM,
-                "message.tiletools.growth_accelerator_info",
-                "message.tiletools.redstone_disable"
-        );
-    
-        registration.addIngredientInfo(
-                new ItemStack(Registration.DECELERATOR.item()),
-                VanillaTypes.ITEM,
-                "message.tiletools.decelerator_info",
-                "message.tiletools.redstone_disable"
-        );
-    
-        registration.addIngredientInfo(
-                new ItemStack(Registration.FREEZER.item()),
-                VanillaTypes.ITEM,
-                "message.tiletools.freezer_info",
-                "message.tiletools.redstone_disable"
-        );
-        
-        ItemStack protectTeBook = new ItemStack(Items.ENCHANTED_BOOK);
-        Map<Enchantment, Integer> protectTe = new HashMap<>();
-        protectTe.put(Registration.PROTECT_TE_ENCHANTMENT.get(), 1);
-        EnchantmentHelper.setEnchantments(protectTe, protectTeBook);
-        registration.addIngredientInfo(
-                protectTeBook,
-                VanillaTypes.ITEM,
-                "message.tiletools.protect_te_info"
-        );
-    
-        registration.addIngredientInfo(
-                new ItemStack(Registration.TEMPORARY_MELTER.get()),
-                VanillaTypes.ITEM,
-                "message.tiletools.melter_info1",
-                "message.tiletools.melter_info2"
-        );
-    
-        registration.addIngredientInfo(
-                new ItemStack(Registration.PORTABLE_FREEZER.get()),
-                VanillaTypes.ITEM,
-                "message.tiletools.portable_freezer_info1",
-                "message.tiletools.portable_freezer_info2"
-        );
+    private static String[] toMessageNames(JsonElement element) {
+        JsonArray array = element.getAsJsonArray();
+        String[] ret = new String[array.size()];
+        for(int i = 0; i < ret.length; i++) {
+            ret[i] = "message.tiletools." + array.get(i).getAsString();
+        }
+        return ret;
     }
 }
